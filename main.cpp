bbd729c1c1fb5a0c503b204ef9dc3926cabd0079
STANISLAV LEBID <75018518+Stanislav-lebid@users.noreply.github.com>
2023-08-03 18:18:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
   
-    std::string filename = "name.txt"; // Замініть це ім'я файлу \ Replace this with the filename you want to search for.
+    std::string filename = "name.txt"; // Замініть ім'я файлу \ Replace this with the filename you want to search for.
     std::string foundFilePath = SearchLibrary::searchFile(filename);
 
     return 0;
